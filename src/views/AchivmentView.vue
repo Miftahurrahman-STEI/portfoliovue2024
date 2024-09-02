d9c018065dd82e3998b24ee00e335eb219945b7f
Miftahurrahman-STEI <mippganz@gamil.com>
2024-09-03 00:16:37 +0700

    Update code syntax page achivment for all images

--- src/views/AchivmentView.vue
+++ src/views/AchivmentView.vue
@@ -1,48 +1,93 @@
 <template>
-    <div class="achivment"
-        v-motion
-        :initial="{ opacity: 0, y: 10 }"
-        :visibleOnce="{ opacity: 1, y: 0 }"
-    >
-            <div class="carousel">
+     <div class="achivment">
+            <h1>Achivment</h1>
+            <div class="carousel"
+                v-motion
+                :initial="{ opacity: 0, y: 10 }"
+                :visibleOnce="{ opacity: 1, y: 0 }"
+            >
 
-                <div v-for="card in cards" :key="card.id" class="card">
-                    <img :src="card.imageUrl" alt="Card Image">
+                <div class="card">
+                    <img src="/public/achivment/achiv.jpeg" />
                     <div class="card-body">
-                        <h3>{{ card.title }}</h3>
-                        <p>{{ card.description }}</p>
+                        <h3>Juara 1 Algoritma Kompetisi Pemrograman C/C++</h3>
+                        <p>Universitas Budi Luhur</p>
                     </div>
                 </div>
-                
+                <div class="card">
+                    <img src="/public/achivment/achiv2.jpg" />
+                    <div class="card-body">
+                        <h3>Peserta Lomba CSS E-Sports Battle WPU EVENT OF THE YEAR</h3>
+                        <p>Universitas Pasundan Bandung</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="/public/achivment/achiv4.jpg" />
+                    <div class="card-body">
+                        <h3>AWS Cloud Competition For Sagasitas Tech 2024</h3>
+                        <p>AWS Sagasitas For Tech 2024</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="/public/achivment/achiv5.jpg" />
+                    <div class="card-body">
+                        <h3>Praktik Kerja Lapangan Komisi Pemilihan Umum</h3>
+                        <p>Komisi Pemilihan Umum</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="/public/achivment/achiv3.jpg" />
+                    <div class="card-body">
+                        <h3>Memulai Dasar Pemrograman untuk Menjadi Pengembang Software</h3>
+                        <p>Dicoding Indonesia</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="/public/achivment/achiv9.jpg" />
+                    <div class="card-body">
+                        <h3>Belajar Dasar Structured Query Language ( SQL )</h3>
+                        <p>Dicoding Indonesia</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="/public/achivment/achiv7.jpg" />
+                    <div class="card-body">
+                        <h3>Belajar Dasar AI (Artificial Intelligence)</h3>
+                        <p>Dicoding Indonesia</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="/public/achivment/achiv8.jpg" />
+                    <div class="card-body">
+                        <h3>Memulai Pemrograman dengan Python</h3>
+                        <p>Dicoding Indonesia</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="/public/achivment/achiv11.jpg" />
+                    <div class="card-body">
+                        <h3>Memulai Pemrograman dengan C</h3>
+                        <p>Dicoding Indonesia</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="/public/achivment/achiv6.jpg" />
+                    <div class="card-body">
+                        <h3>Dicoding Developer Coaching #103</h3>
+                        <p>Dicoding Indonesia</p>
+                    </div>
+                </div>
+                <div class="card">
+                    <img src="/public/achivment/achiv10.jpg" />
+                    <div class="card-body">
+                        <h3>Javascript 10 Projects for Beginner</h3>
+                        <p>Udemy Course</p>
+                    </div>
+                </div>
+
             </div>
         </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path fill="#FCEDE6" fill-opacity="1" d="M0,192L34.3,186.7C68.6,181,137,171,206,181.3C274.3,192,343,224,411,218.7C480,213,549,171,617,160C685.7,149,754,171,823,160C891.4,149,960,107,1029,80C1097.1,53,1166,43,1234,58.7C1302.9,75,1371,117,1406,138.7L1440,160L1440,0L1405.7,0C1371.4,0,1303,0,1234,0C1165.7,0,1097,0,1029,0C960,0,891,0,823,0C754.3,0,686,0,617,0C548.6,0,480,0,411,0C342.9,0,274,0,206,0C137.1,0,69,0,34,0L0,0Z">
-            </path>
-        </svg>
 </template>
-
-<script>
-    export default{
-        data() {
-            return {
-                cards: [
-                    { id: 1, imageUrl: '/public/achivment/achiv.jpeg',title: 'Juara 1 Algoritma Kompetisi Pemrograman C/C++', description: 'Universitas Budi Luhur' },
-                    { id: 2, imageUrl: '/public/achivment/achiv2.jpg',title: 'Peserta Lomba CSS E-Sports Battle WPU EVENT OF THE YEAR', description: 'Universitas Pasundan Bandung' },
-                    { id: 3, imageUrl: '/public/achivment/achiv4.jpg',title: 'AWS Cloud Competition For Sagasitas Tech 2024', description: 'AWS Sagasitas For Tech 2024' },
-                    { id: 4, imageUrl: '/public/achivment/achiv5.jpg',title: 'Praktik Kerja Lapangan Komisi Pemilihan Umum', description: 'Komisi Pemilihan Umum'},
-                    { id: 5, imageUrl: '/public/achivment/achiv3.jpg',title: 'Memulai Dasar Pemrograman untuk Menjadi Pengembang Software', description: 'Dicoding Indonesia' },
-                    { id: 6, imageUrl: '/public/achivment/achiv9.jpg',title: 'Belajar Dasar Structured Query Language ( SQL )' , description: 'Dicoding Indonesia'},
-                    { id: 7, imageUrl: '/public/achivment/achiv7.jpg',title: 'Belajar Dasar AI (Artificial Intelligence)', description: 'Dicoding Indonesia' },
-                    { id: 8, imageUrl: '/public/achivment/achiv8.jpg',title: 'Memulai Pemrograman dengan Python', description: 'Dicoding Indonesia' },
-                    { id: 9, imageUrl: '/public/achivment/achiv11.jpg',title: 'Memulai Pemrograman dengan C', description: 'Dicoding Indonesia'},
-                    { id: 10, imageUrl:'/public/achivment/achiv6.jpg',title: 'Dicoding Developer Coaching #103', description: 'Dicoding Indonesia'},
-                    { id: 11, imageUrl:'/public/achivment/achiv10.jpg',title: 'Javascript 10 Projects for Beginner', description: 'Udemy Course'},
-                ],
-            }
-        }
-    }
-</script>
 
 <style scoped>
          .achivment{
