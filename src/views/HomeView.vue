2d3e1eacbf53088d84ac483ae2d1c04e4d6678c7
Miftahurahman-STEI <mippganz@gmail.com>
2024-12-25 03:08:43 +0700

    feat: remove categories card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -209,12 +209,12 @@ import { ref } from 'vue';
                     <img src="/src/assets/achivment/achiv-64f44558.jpeg" />
                     <article class="card-body text-wrap">
                         <h3>Juara 1 Algoritma Kompetisi Pemrograman C/C++</h3>
-                        <div>
+                        <!-- <div>
                             <ul class="flex gap-4 *:rounded-full *:border *:border-sky-100 *:bg-sky-50 *:px-2 *:py-0.5 w-10 dark:text-sky-300 dark:*:border-sky-500/15 dark:*:bg-sky-500/10">
                                 <li>Sales</li>
                                 <li>Marketing</li>
                             </ul>
-                        </div>
+                        </div> -->
                     </article>
                 </div>
                 <div class="card">
